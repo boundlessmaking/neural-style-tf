fd7e66f4a755a5fd9167ee4aa9e554ea304406f7
Robin Godwyll <rodwyll+github@gmail.com>
2019-10-31 19:56:51 +0100

    seems to run fine

--- running_for_robin.py
+++ running_for_robin.py
@@ -1,45 +1,51 @@
-import run_main
+#import run_main
 import os
 
 
 
 
 
-results_path = "/Users/robin1/Projects/AI Depth maps/python_style_transf/neural-style-tf/image_output/result03"
+results_path = "/Users/robin1/Projects/AI_Depth_maps/python_style_transf/neural-style-tf/image_output/result03/"
 
-style_path = "/Users/robin1/Projects/AI Depth maps/python_style_transf/neural-style-tf/styles03"
-pattern_path = "/home/liiru/ai/tensorflow-style-transfer-master/hyper/patterns/"
+style_path = "/Users/robin1/Projects/AI_Depth_maps/python_style_transf/neural-style-tf/styles03/"
+pattern_path = "/Users/robin1/Projects/AI_Depth_maps/python_style_transf/neural-style-tf/image_input/"
 
 
 
 #these initialize the lists of styles and patterns in respective folders
 styles = []
+print(os.listdir(style_path))
 
 for filename in os.listdir(style_path):
-    if filename.endswith("jpeg") or filename.endswith("jpg"):
+    if filename.endswith("jpg") or filename.endswith("png"):
         # Your code comes here such as
         print(filename)
         styles.append(filename)
 
+print(styles)
+
 patterns = []
 
+print(os.listdir(pattern_path))
+
 for filename in os.listdir(pattern_path):
-    if filename.endswith("jpeg") or filename.endswith("jpg"):
+    if filename.endswith("jpg") or filename.endswith("png"):
         # Your code comes here such as
         print(filename)
         patterns.append(filename)
 
-
+print(patterns)
 
 
 #this gives the system the run commands
 def run_helper(content, style, output_path, loss):
-    run_string = "python neural_style.py --content_img "+ content + " --style_imgs " + style + " --img_output_dir " + output_path + " --style_weight " + loss +" --max_iterations 1000"
+    run_string = "python neural_style.py --content_img "+ content + " --style_imgs " + style + " --img_output_dir " + output_path + " --style_weight " + loss +" --max_iterations 1000" + " --device /cpu:0 "
+    print(run_string)
     return run_string
 
 
 #loss ratios to be tested
-loss_ratios = ["1e6","1e4","1e2","1","1e-2","1e-4","1e-6"]
+loss_ratios = ["1e4","1e2","1","1e-2","1e-4"]
 
 
 #run this to do the stuff
@@ -49,7 +55,9 @@ for pattern in patterns:
             tyyli_path = style_path + tyyli
             content_path = pattern_path + pattern
             output_path = results_path + pattern.split(".")[0] + "_" + tyyli + "_" + loss +"_t1"
+            print(content_path, tyyli_path, output_path, loss)
             try:
+                print("running files")
                 os.system(run_helper(content_path, tyyli_path, output_path, loss))
             except:
                 print("some error happened")
